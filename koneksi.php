2bd95a82dbae163a5bf93cd7e7deccaa52c044df
Moch. Fattahul Alim <mfalim13@gmail.com>
2026-04-14 14:51:54 +0700

    koneksi db

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+    $host = "localhost";
+    $username  = "root";
+    $password = "";
+    $database = "db_proyek";
+
+    $conn = mysqli_connect($host, $username, $password, $database);
+    if (!$conn) {
+        die("Koneksi gagal: " . mysqli_connect_error());
+    }
